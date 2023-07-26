b9403fb62cb1f5220c20cc66c4472b1995244844
Florian SIMUNEK <florian.simunek@gmail.com>
2023-07-26 21:55:18 +0200

    :sparkles: Add handleChange for Task

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,6 +1,34 @@
+/* IMPORTS */
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { toggleTask } from '@/store/todosSlice';
+
 /* CSS */
 import styles from './Task.module.scss';
 
 export default function Task({ task }) {
-    return <p className={styles.task}>{task.text}</p>;
+    const dispatch = useDispatch();
+    const [isDone, setIsDone] = useState(false);
+
+    const handleChange = (id) => {
+        dispatch(toggleTask(id));
+        setIsDone((done) => !done);
+    };
+
+    const taskDone = isDone ? styles.taskDone : '';
+
+    return (
+        <div className={`${styles.task} ${taskDone}`}>
+            <div className={styles.task__left}>
+                <input
+                    type="checkbox"
+                    name="toggleTask"
+                    id="toggleTask"
+                    checked={task.done}
+                    onChange={() => handleChange(task.id)}
+                />
+                <p>{task.text}</p>
+            </div>
+        </div>
+    );
 }
