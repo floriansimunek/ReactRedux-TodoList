64f73ddbb16a07f946073aac4dd82f705c7e6452
Florian SIMUNEK <florian.simunek@gmail.com>
2023-07-26 10:36:33 +0200

    :wrench: Basic configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+/* DEPENDENCIES */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+/* CSS */
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <Provider>
+    <React.StrictMode></React.StrictMode>
+  </Provider>
+);
